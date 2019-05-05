427b797310bff774ec2e89a2deaf3c1e68683631
Bodgar Espinosa Miranda <bodgarjairespinosamiranda@2806-106e-0020-02ca-04b8-8e3b-5ce1-deeb.ipv6.infinitum.net.mx>
2019-05-05 13:19:11 -0500

    Initial Commit

---
+++ Fitness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fitness
+//
+//  Created by bodgar jair espinosa miranda on 05/05/19.
+//  Copyright © 2019 bodgar jair espinosa miranda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
